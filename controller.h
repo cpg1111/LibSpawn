8ccc6ec55e43d025a0807638c4e0fad5dfb5d5fa
Christian Grabowski <christiang11754@gmail.com>
2016-03-25 21:42:14 -0400

    WIP spawn

--- controller.h
+++ controller.h
@@ -1,10 +1,60 @@
+#include <errno.h>
 #include <stdio.h>
-#include <threads.h>
+#include <string.h>
+#include <sys/types.h>
+#include <signal.h>
 #include <unistd.h>
 
 #ifndef CONTROLLER_H_
 #define CONTROLLER_H_
-  typedef struct Controller {
-    
+
+  extern int errno;
+
+  typedef struct
+  {
+    pid_t child;
+    pid_t grandChild;
+    char* status;
+    int code;
+    char* args[];
+    char* env[];
+  } Controller;
+
+  Controller *newController(char* args[], char* env[], char* status)
+  {
+    Controller ctl = {NULL, NULL, args, env, "spawning", NULL};
+    return &ctl;
   }
+
+  int spawn(Controller* ctl)
+  {
+    pid_t parent = getpid();
+    pid_t pid = fork();
+    if (pid == -1)
+    {
+      int errnum = errno;
+      perror("could not spawn child process");
+      fprintf(stderr, "Error spawning child process %s\n", stderr(errnum));
+    }
+    else if(pid > 0)
+    {
+      return 1;
+    }
+    else
+    {
+      if(ctl->child == NULL)
+      {
+        ctl->child = pid;
+        return spawn(ctl);
+      }
+      else if(ctl->grandChild == NULL)
+      {
+        ctl->grandChild = pid;
+        execve(ctl->args[0], ctl->args, ctl->env);
+        kill(ctl->child, SIGTERM);
+        exit(EXIT_FAILURE);
+      }
+    }
+  }
+
 #endif
--- spawn.c
+++ spawn.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+#include "controller.h"
 
-int main(int argc, char *argv[]){
-  
+int main(int argc, char *argv[])
+{
+  Controller* ctl = newController("echo \"hello world\"", NULL, "spawning");
+  return spawn(ctl);
 }
